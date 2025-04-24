100ad2bf4e99e1e8ef90450c4e76429f96764e8e
Miguel de Icaza <miguel@gnome.org>
2025-04-24 10:34:07 -0400

    Initial Commit

---
+++ SwiftGodotKitSamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftGodotKitSamples
+//
+//  Created by Miguel de Icaza on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftGodotKitSamples/SwiftGodotKitSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGodotKitSamplesApp.swift
+//  SwiftGodotKitSamples
+//
+//  Created by Miguel de Icaza on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGodotKitSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
