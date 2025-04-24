e7b6a84e4ddbf3f5583ace5ff530af8a22fd6d83
Miguel de Icaza <miguel@gnome.org>
2025-04-24 16:37:54 -0400

    Make it run

--- SwiftGodotKitSamples/ContentView.swift
+++ SwiftGodotKitSamples/ContentView.swift
@@ -4,16 +4,27 @@
 //
 //  Created by Miguel de Icaza on 4/24/25.
 //
-
+import Foundation
 import SwiftUI
+import SwiftGodotKit
 
 struct ContentView: View {
+    @State var app: GodotApp = GodotApp(packFile: "main.pck")
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("The image below is a Godot game:")
+            GodotAppView()
+                .environment(\.godotApp, app)
+        }
+        .onAppear {
+            let dir = Bundle.main.resourcePath ?? "."
+            let file = "\(dir)/main.pck"
+            let p = try! Data(contentsOf: URL(filePath: file))
+            print("path is: \(Bundle.main.resourcePath)")
+            let e = FileManager.default.enumerator(atPath: Bundle.main.resourcePath ?? ".")
+            while let file = e?.nextObject() {
+                print (file)
+            }
         }
         .padding()
     }
